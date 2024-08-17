fc807deffeecfe8a2cf5744f1a95bf459737b532
Bohdan Vivchar <bogdanvivchar8@gmail.com>
2024-08-17 21:47:11 +0300

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,16 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      "best-blue": "#0F0BCB",
-      "best-pink": "#EC9EFF",
-      "best-orange": "#FFAF75",
-      "best-green": "#77DD77",
-      "best-yellow": "#FFF16C",
-      "best-red": "#FF617E",
-      "best-purple": "#EC9EFF"
+    extend: {
+      colors: {
+        "best-blue": "#0F0BCB",
+        "best-pink": "#EC9EFF",
+        "best-orange": "#FFAF75",
+        "best-green": "#77DD77",
+        "best-yellow": "#FFF16C",
+        "best-red": "#FF617E",
+        "best-purple": "#EC9EFF"
+      }
     }
   },
   plugins: [],
