e68d582642f7a53518a5e823e2464b18a63e5cb9
pullingkids <mimagdych@gmail.com>
2024-08-17 23:39:40 +0300

    color2

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import { useTranslations } from "next-intl";
+import Clr_Sqrs_full from "./components/Clr_Sqrs_full";
 
 export default function Home() {
   const t = useTranslations("Home");
   
   return (
     <main>
-      <p>{t('Hi')}</p>
+      
+    <Clr_Sqrs_full></Clr_Sqrs_full>
     </main>
   )
 }
