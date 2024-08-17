9e089b90a1efb4c5192e6d16b8b7e409eb5eb548
YuliaSinichak <yuliasinichak@gmail.com>
2024-08-17 20:55:47 +0300

    footer started

---
+++ app/[locale]/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer(){
+    return (
+        <div className="w-full min-h-80 bg-best-blue "> 
+            <h1 className="text-4xl">Welcome!</h1>
+        </div>
+    )
+}
