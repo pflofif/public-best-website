8d6f8f13fc45d2eb763587fbe846d7f25f75c712
Bohdan Vivchar <bogdanvivchar8@gmail.com>
2024-08-17 20:52:47 +0300

    add default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,16 @@ module.exports = {
     '.app/components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    colors: {
+      "best-blue": "#0F0BCB",
+      "best-pink": "#EC9EFF",
+      "best-orange": "#FFAF75",
+      "best-green": "#77DD77",
+      "best-yellow": "#FFF16C",
+      "best-red": "#FF617E",
+      "best-purple": "#EC9EFF"
+    }
+  },
   plugins: [],
 }
