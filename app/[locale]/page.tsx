1a666a731106f6de42d7f84254ceaf8835c44dfb
YuliaSinichak <yuliasinichak@gmail.com>
2024-08-18 02:48:04 +0300

    OurPartners section added

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,14 +1,25 @@
 "use client"
 import { useTranslations } from "next-intl";
 import FailedMessage from "./components/FailedMessage";
+import OurPartners from "./components/OurPartners";
 import Footer from "./components/Footer";
+import Image from "next/image";
 
 export default function Home() {
   const t = useTranslations("Home");
 
   return (
     <main>
-  
+      <div className="relative">
+        <OurPartners />
+        <Image
+            src="/Partners_bg.svg"
+            layout="fill"
+            objectFit="cover"
+            className="w-full h-full -z-20 opacity-90"
+            alt=""
+        />
+      </div>
       <Footer />
     </main>
   )
