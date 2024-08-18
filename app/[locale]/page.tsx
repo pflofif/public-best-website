b84ad7ed131796bb2fec475aace547863fa2d13b
AnnaHashchuk1 <annahashchuk@gmail.com>
2024-08-18 04:04:04 +0300

    Timeline section added

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useTranslations } from "next-intl";
+import Timeline from './components/Timeline'
 
 export default function Home() {
   const t = useTranslations("Home");
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main>
       <p>{t('Hi')}</p>
+      <Timeline/>
     </main>
   )
 }
