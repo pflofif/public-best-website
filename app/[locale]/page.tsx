373af3e9447e57a9a6464652d9367d42e5d0e3e0
Bohdan Vivchar <bogdanvivchar8@gmail.com>
2024-09-05 07:54:26 +0300

    add WhatIsBest component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,37 +1,30 @@
 "use client"
-import {useTranslations} from "next-intl";
+import { useTranslations } from "next-intl";
 
 import Timeline from './components/Timeline'
-import Event from "./components/EventSection/Event";
-import FailedMessage from "./components/FailedMessage";
 import OurPartners from "./components/OurPartners";
-import Footer from "./components/Footer";
 import Image from "next/image";
 import Header from "./components/Header";
 import WeDevelopStudents from "./components/WeDevelopStudents";
 import ValuesPage from "./components/DepartmentsAndIdentity/ColorSquersValuesFull";
 import EventSliderSection from "./components/EventSliderSection/EventSliderSection";
 import HowOrganizationWork from "./components/HowOrganizationWork";
-import BoardTitle from "./components/BoardTitle";
-import RectangleBoard from "./components/ServicesAndBoard/RectangleBoard";
+import WhatIsBest from "./components/WhatIsBest";
 
 export default function Home() {
     const t = useTranslations("Home");
     const events = useTranslations("Events");
     return (
         <main>
-            <Header/>
-            <WeDevelopStudents/>
-            <RectangleBoard></RectangleBoard>
+            <Header />
+            <WeDevelopStudents />
             <div className="relative">
-                <Event description="loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem " heading="kja;sd" sectionColor="best-blue" imageName="bec" ></Event>
-                <Event description="loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem " heading="kja;sd" sectionColor="best-blue" imageName="bec" isInverted></Event>
-                <Event description="loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem loreem " heading="kja;sd" sectionColor="best-blue" imageName="bec" ></Event>
-                <Timeline/>
-                <ValuesPage/>
-                <EventSliderSection/>
-                <HowOrganizationWork/>
-                <OurPartners/>
+                <WhatIsBest />
+                <Timeline />
+                <ValuesPage />
+                <EventSliderSection />
+                <HowOrganizationWork />
+                <OurPartners />
                 <Image
                     src="/Partners_bg.svg"
                     layout="fill"
