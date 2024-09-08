4acb71d57f1aa53c2fce8c17231a76bec5423b8d
VGcod3 <vktrgavryliuk@gmail.com>
2024-09-08 22:28:14 +0300

    fixes

--- app/[locale]/components/Boardies.tsx
+++ app/[locale]/components/Boardies.tsx
@@ -1,10 +1,12 @@
+"use client";
 import Image from "next/image";
 import { maruipol_bold, inter } from "../../fonts/fonts";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation, Keyboard } from "swiper/modules";
+import { useState, useEffect } from "react";
+import { Swiper as SwiperType } from "swiper/types";
 
 const boardiesData = [
   {
@@ -91,48 +93,45 @@ const Boardie = ({
 };
 
 const BoardieMobile = ({
-    imgSrc,
-    alt,
-    name,
-    description,
-  }: {
-    imgSrc: string;
-    alt: string;
-    name: string;
-    description: string;
-  }) => {
-    return (
-      <div className="flex flex-col justify-between rounded-xl shadow-lg hover:scale-105 duration-500 p-4 w-60 bg-white mx-auto my-8">
-        <Image
-          src={imgSrc}
-          alt={alt}
-          layout="responsive"
-          width={300}
-          height={300}
-          className="w-full h-56 rounded-xl"
-        />
-        <div className="flex flex-col justify-center items-center p-2 flex-grow">
-          <h1 className={`${maruipol_bold.className} text-xl text-center`}>
-            {name}
-          </h1>
-          <div className="p-2 px-4 bg-red-200 rounded-3xl mt-2">
-            <p className={`${inter.className} text-center text-xs bg-red-200`}>
-                {description}
-            </p>
-          </div>
-          
+  imgSrc,
+  alt,
+  name,
+  description,
+}: {
+  imgSrc: string;
+  alt: string;
+  name: string;
+  description: string;
+}) => {
+  return (
+    <div className="flex flex-col justify-between rounded-xl shadow-lg hover:scale-105 duration-500 p-4 w-60 bg-white mx-auto my-8">
+      <Image
+        src={imgSrc}
+        alt={alt}
+        layout="responsive"
+        width={300}
+        height={300}
+        className="w-full h-56 rounded-xl"
+      />
+      <div className="flex flex-col justify-center items-center p-2 flex-grow">
+        <h1 className={`${maruipol_bold.className} text-xl text-center`}>
+          {name}
+        </h1>
+        <div className="p-2 px-4 bg-red-200 rounded-3xl mt-2">
+          <p className={`${inter.className} text-center text-xs bg-red-200`}>
+            {description}
+          </p>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  
-
-export default function Boardies() { 
- return (
+export default function Boardies() {
+  return (
     <div className="min-h-screen flex items-center justify-center mt-8 lg:mt-40 w-full">
       {/* Desktop View */}
-      <div className="gap-4 flex flex-wrap w-full justify-center my-8 max-w-6xl hidden md:flex">
+      <div className="gap-4 flex-wrap w-full justify-center my-8 max-w-6xl hidden md:flex">
         {boardiesData.map((p) => (
           <Boardie
             key={p.name}
@@ -143,36 +142,119 @@ export default function Boardies() {
           />
         ))}
       </div>
-
       {/* Mobile View */}
-      <div className="md:hidden w-full flex justify-center items-center">
-        <Swiper
-          spaceBetween={100}
-          centeredSlides={true}
-          autoplay={{
-            delay: 2000,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-            type: 'custom'
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="flex items-center justify-center mx-auto w-full"
-        >
-          {boardiesData.map((p) => (
-            <SwiperSlide key={p.name}>
-              <BoardieMobile
-                imgSrc={p.imgSrc}
-                alt={p.alt}
-                name={p.name}
-                description={p.description}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <SwiperWrapper />
     </div>
   );
 }
+
+const numberOfSlides = boardiesData.length;
+
+const SwiperWrapper = () => {
+  const [activeSlide, setActiveSlide] = useState<number>(0);
+  const [swiper, setSwiper] = useState<SwiperType>();
+
+  const handleChangeSlide = (slideIndex: number) => {
+    const newIndex = (slideIndex + numberOfSlides) % numberOfSlides;
+
+    swiper?.slideTo(newIndex);
+    setActiveSlide(newIndex);
+  };
+
+  useEffect(() => {
+    handleChangeSlide(0);
+  }, []);
+  return (
+    <div className="md:hidden w-full flex justify-evenly items-center px-4 sm:px-10">
+      <button
+        onClick={() => handleChangeSlide(activeSlide - 1)}
+        className="text-2xl  bg-white p-2 rounded-full text-neutral-400 shadow-lg"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-neutral-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15 19l-7-7 7-7"
+          ></path>
+        </svg>
+      </button>
+      <BoardiesSwiper
+        activeSlide={activeSlide}
+        setActiveSlide={setActiveSlide}
+        setSwiper={setSwiper}
+        swiper={swiper}
+      />
+
+      <button
+        className="text-2xl bg-white p-2 rounded-full shadow-lg text-neutral-400"
+        onClick={() => handleChangeSlide(activeSlide + 1)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 "
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 5l7 7-7 7"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+const BoardiesSwiper = ({
+  activeSlide,
+  setActiveSlide,
+  setSwiper,
+  swiper,
+}: {
+  activeSlide: number;
+  setActiveSlide: (slideIndex: number) => void;
+  swiper?: SwiperType;
+  setSwiper: ((swiper: SwiperType) => void) | undefined;
+}) => {
+  return (
+    <Swiper
+      initialSlide={activeSlide}
+      onSlideChange={() => setActiveSlide(swiper?.realIndex as number)}
+      onSwiper={setSwiper}
+      spaceBetween={100}
+      centeredSlides={true}
+      loop={true}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+      }}
+      navigation={true}
+      keyboard={{
+        enabled: true,
+      }}
+      modules={[Autoplay, Navigation, Keyboard]}
+      className="flex items-center justify-center mx-auto w-full"
+    >
+      {boardiesData.map((p) => (
+        <SwiperSlide key={p.name}>
+          <BoardieMobile
+            imgSrc={p.imgSrc}
+            alt={p.alt}
+            name={p.name}
+            description={p.description}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
--- app/[locale]/components/Header.tsx
+++ app/[locale]/components/Header.tsx
@@ -1,124 +1,166 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import Image from 'next/image'
+import { useEffect, useState } from "react";
+import Image from "next/image";
 import { inter } from "../../fonts/fonts";
 import Link from "next/link";
 import { cn } from "../../lib/utils";
 
-export default function Header() {
-    const [isOpen, setIsOpen] = useState(false);
+const menuItems = [
+  { label: "Про нас", href: "/about-us" },
+  {
+    label: "Блог",
+    href: "/blog",
+    isDropdown: true,
+    dropdownItems: [
+      { label: "Blog 1", href: "#" },
+      { label: "Blog 2", href: "#" },
+    ],
+  },
+  { label: "Івенти", href: "/events" },
+  { label: "Твої можливості", href: "/opportunities" },
+  { label: "Галерея", href: "/gallera" },
+  { label: "Q/A", href: "/qa" },
+];
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
-    const menuItems = [
-        { label: 'Про нас', href: '/about-us' },
-        {
-            label: 'Блог',
-            href: '/blog',
-            isDropdown: true,
-            dropdownItems: [
-                { label: 'Blog 1', href: '#' },
-                { label: 'Blog 2', href: '#' },
-            ],
-        },
-        { label: 'Івенти', href: '/events' },
-        { label: 'Твої можливості', href: '/opportunities' },
-        { label: 'Галерея', href: '/gallera' },
-        { label: 'Q/A', href: '/qa' },
-    ];
-
-    return (
-        <header className={`${inter.className} bg-best-blue text-white fixed top-0 left-0 w-full min-h-28 z-50 text-[20px] px-6 md:px-12`}>
-            <div className="mx-auto flex justify-between items-center gap-x-5 h-[110px]">
-                {/* Logo */}
-                <Link href="/" className="flex items-center basis-32 shrink-0 z-50">
-                    <img src="/best-logo.svg" alt="Best Lviv" width={123} height={64}
-                        onClick={() => setIsOpen(false)}
-                        className="max-h-12 lg:min-h-48" />
+const MenuItems = ({ setIsOpen }: { setIsOpen: (arg0: boolean) => void }) => {
+  return (
+    <>
+      {menuItems.map((item, index) =>
+        !item.isDropdown ? (
+          <li key={index}>
+            <Link
+              onClick={() => setIsOpen(false)}
+              href={item.href}
+              className="hover:text-gray-300 "
+            >
+              {item.label}
+            </Link>
+          </li>
+        ) : (
+          <li key={index} className="relative group cursor-pointer">
+            <button className="hover:text-gray-300 flex items-center lg:pb-2">
+              {item.label}
+              <svg
+                className="ml-1 w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
+            </button>
+            {
+              //Dropdown Menu
+            }
+            <div className="min-lg:absolute left-0 py-2 w-40 min-lg:bg-blue-800 min-lg:rounded-md min-lg:shadow-lg hidden min-lg:group-hover:opacity-100 group-hover:block min-lg:group-hover:visible transition duration-200 ease-in-out">
+              {item.dropdownItems.map((dropdownItem, i) => (
+                <Link
+                  key={i}
+                  href={dropdownItem.href}
+                  onClick={() => setIsOpen(false)}
+                  className="block px-4 py-2 max-lg:text-2xl text-sm hover:bg-blue-700"
+                >
+                  {dropdownItem.label}
                 </Link>
-
-                {/*Menu */}
-                <nav className={cn(`max-lg:absolute max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:h-svh min-lg:relative min-lg:flex basis-full justify-center`)}>
-                    <ul className={cn("max-lg:absolute max-lg:top-0 max-lg:left-full max-lg:w-full flex max-lg:text-3xl max-lg:flex-col max-lg:justify-start max-lg:bg-best-blue gap-y-7 max-lg:h-svh max-lg:p-10 max-lg:pt-36 justify-center gap-x-10 2xl:gap-x-20 transition-all duration-500", isOpen && "max-lg:left-1/4")}>
-                        {menuItems.map((item, index) =>
-                            item.isDropdown ? (
-                                <li key={index} className="relative group cursor-pointer">
-                                    <button className="hover:text-gray-300 flex items-center min-lg:pb-2">
-                                        {item.label}
-                                        <svg
-                                            className="ml-1 w-4 h-4"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            viewBox="0 0 24 24"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M19 9l-7 7-7-7"
-                                            ></path>
-                                        </svg>
-                                    </button>
-                                    {/* Dropdown Menu */}
-                                    <div className="min-lg:absolute left-0 py-2 w-40 min-lg:bg-blue-800 min-lg:rounded-md min-lg:shadow-lg hidden min-lg:group-hover:opacity-100 group-hover:block min-lg:group-hover:visible transition duration-200 ease-in-out">
-                                        {item.dropdownItems.map((dropdownItem, i) => (
-                                            <Link
-                                                key={i}
-                                                href={dropdownItem.href}
-                                                onClick={() => setIsOpen(false)}
-                                                className="block px-4 py-2 max-lg:text-2xl text-sm hover:bg-blue-700"
-                                            >
-                                                {dropdownItem.label}
-                                            </Link>
-                                        ))}
-                                    </div>
-                                </li>
-                            ) : (
-                                <li className="">
-                                    <Link
-                                        onClick={() => setIsOpen(false)}
-                                        key={index}
-                                        href={item.href}
-                                        className="hover:text-gray-300 "
-                                    >
-                                        {item.label}
-                                    </Link>
-                                </li>
-                            )
-                        )}
-                    </ul>
-
-                </nav>
-
-                {/* Stand with Ukraine button */}
-                <Link href={"#"} className="hidden min-lg:block basis-64 shrink-0">
-                    <div className="bg-white text-best-blue px-4 py-2 rounded-full font-bold flex items-center">
-                        Stand with Ukraine
-                        <img
-                            src="/ukrainianFlag.svg"
-                            alt="UK"
-                            width={32}
-                            height={32}
-                            className="ml-2"
-                        />
-                    </div>
-                </Link>
-
-                {/* Mobile Menu Button */}
-                <div className="min-lg:hidden z-50">
-                    <button onClick={toggleMenu} className={cn("focus:outline-none transition-all duration-500", isOpen && "rotate-90 ")}>
-
-                        <Image src="/headerBurger.svg" alt="dhjfdh" width={42} height={42} />
-
-                    </button>
-                </div>
+              ))}
             </div>
+          </li>
+        ),
+      )}
+    </>
+  );
+};
 
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
 
-        </header>
-    );
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  // To block scrolling when the menu is open
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isOpen]);
+
+  return (
+    <header
+      className={`${inter.className} bg-best-blue text-white fixed top-0 left-0 w-full min-h-28 z-50 text-[20px] px-6 md:px-12`}
+    >
+      <div className="mx-auto flex justify-between items-center gap-x-5 h-[110px]">
+        {/* Logo */}
+        <Link href="/" className="flex items-center basis-32 shrink-0 z-50">
+          <img
+            src="/best-logo.svg"
+            alt="Best Lviv"
+            width={123}
+            height={64}
+            onClick={() => setIsOpen(false)}
+            className="max-h-12 lg:min-h-48"
+          />
+        </Link>
+
+        {/* Desktop menu */}
+        <nav className="hidden xl:flex">
+          <ul className="flex gap-x-10">
+            <MenuItems setIsOpen={setIsOpen} />
+          </ul>
+        </nav>
+        {/* Mobile menu */}
+        <nav className="xl:hidden absolute top-0 left-0 w-full h-svh basis-full justify-center pointer-events-none">
+          <ul
+            className={cn(
+              "absolute top-0 left-full w-full flex text-3xl flex-col justify-start bg-best-blue gap-y-7 h-svh p-10 pt-36  gap-x-10 transition-all duration-500 pointer-events-auto",
+              isOpen && "left-1/4",
+            )}
+          >
+            {<MenuItems setIsOpen={setIsOpen} />}
+          </ul>
+        </nav>
+
+        {/* Stand with Ukraine button */}
+        <Link href={"#"} className="hidden xl:block basis-64 shrink-0">
+          <div className="bg-white text-best-blue px-4 py-2 rounded-full font-bold flex items-center">
+            Stand with Ukraine
+            <img
+              src="/ukrainianFlag.svg"
+              alt="UK"
+              width={32}
+              height={32}
+              className="ml-2"
+            />
+          </div>
+        </Link>
+
+        {/* Mobile Menu Button */}
+        <div className="xl:hidden z-50">
+          <button
+            onClick={toggleMenu}
+            className={cn(
+              "focus:outline-none transition-all duration-500",
+              isOpen && "rotate-90 ",
+            )}
+          >
+            <Image
+              src="/headerBurger.svg"
+              alt="dhjfdh"
+              width={42}
+              height={42}
+            />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 }
