a1d14520b0ce45356d4baa2ff3f2ae4bcf83cfb1
Bohdan Vivchar <bogdanvivchar8@gmail.com>
2024-10-10 01:47:37 +0300

    just for test, use local rusr server

--- app/[locale]/events/page.tsx
+++ app/[locale]/events/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
     const eventsT = useTranslations("Events");
 
     useEffect(() => {
-        fetch("/api/events")
+        fetch("http://localhost:8080/events")
             .then((response) => response.json())
             .then((data) => {
                 setEvents(data);
